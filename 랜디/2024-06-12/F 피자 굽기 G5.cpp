593a582591777c476eff0b4c056118ccec2d2ca6
jemin0619 <72261100+jemin0619@users.noreply.github.com>
2024-06-16 11:03:27 +0900

    Update F 피자 굽기 G5.cpp

--- 랜디/2024-06-12/F 피자 굽기 G5.cpp
+++ 랜디/2024-06-12/F 피자 굽기 G5.cpp
@@ -2,6 +2,9 @@
 //구멍 A 이후의 구멍들에 실질적으로 넣을 수 있는 크기는 A보다 작거나 같을 수밖에 없다.
 //그렇게 Monotonic하게 요소들을 관리해면 O(N)에 해결이 가능하다.
 
+//Monotonic하게 vector에 저장 후 Binary Search 풀이도 가능하다.
+//그것보단 Stack이 깔끔한 듯 하다.
+
 #include <bits/stdc++.h>
 using namespace std;
 #define fastio cin.tie(NULL)->sync_with_stdio(false);
