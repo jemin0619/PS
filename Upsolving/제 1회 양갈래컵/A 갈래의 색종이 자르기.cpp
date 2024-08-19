fd22be4db6bade774cd6fd480d6c0f4d3fa40d84
jemin0619 <72261100+jemin0619@users.noreply.github.com>
2024-08-20 08:15:14 +0900

    Create A 갈래의 색종이 자르기.cpp

---
+++ Upsolving/제 1회 양갈래컵/A 갈래의 색종이 자르기.cpp
@@ -0,0 +1,13 @@
+//처음에 정사각형이였다고 하므로 한 변의 길이를 구하고 4번 더하면 된다.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cin.tie(NULL)->sync_with_stdio(false);
+    int N; cin>>N;
+    N*=2;
+    int X = sqrt(N);
+    cout<<X*4;
+    return 0;
+}
