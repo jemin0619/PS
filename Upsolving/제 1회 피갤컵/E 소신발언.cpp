e1f2851737c77f43991c0d61d5b8dc5535b94e09
jemin0619 <72261100+jemin0619@users.noreply.github.com>
2024-08-24 13:54:32 +0900

    Create E 소신발언.cpp

---
+++ Upsolving/제 1회 피갤컵/E 소신발언.cpp
@@ -0,0 +1,38 @@
+//check가 볼록함수이므로 삼분 탐색으로 해결할 수 있다.
+
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio cin.tie(NULL)->sync_with_stdio(false)
+#define ll long long
+
+int N;
+vector<ll> V;
+
+//x에 히터를 설치했을 때 모든 소의 신발을 녹이는 시간
+ll check(ll x){
+    ll ret = 0;
+    for(int i=x-1; i>=1; i--) ret = max(abs(x-i)*V[i], ret);
+    for(int i=x+1; i<=N; i++) ret = max(abs(x-i)*V[i], ret);
+    return ret;
+}
+
+int main(){
+    fastio;
+    cin>>N;
+    V.resize(N+1);
+    for(int i=1; i<=N; i++) cin>>V[i];
+    
+    ll st = 0; ll en = N+1;
+    while(st+2<en){
+        ll p = (st*2+en)/3;
+        ll q = (st+en*2)/3;
+        if(check(p) <= check(q)) en=q;
+        else st=p;
+    }
+    ll ans = check(max(1LL, st));
+    for(int i=max(1LL,st); i<=en; i++){
+        ans = min(check(i), ans);
+    }
+    cout<<ans;
+    return 0;
+}
