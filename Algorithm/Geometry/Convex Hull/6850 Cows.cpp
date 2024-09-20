b2605efc22d1e32a93aab24b28e3bc8efdae31e7
jemin0619 <72261100+jemin0619@users.noreply.github.com>
2024-09-21 00:05:47 +0900

    Update 6850 Cows.cpp

--- Algorithm/Geometry/Convex Hull/6850 Cows.cpp
+++ Algorithm/Geometry/Convex Hull/6850 Cows.cpp
@@ -4,53 +4,68 @@
 //34번의 제출 끝에 원래 짜려던 코드를 작성하고 AC를 받아낼 수 있었다...
 
 #include <bits/stdc++.h>
-#define fastio cin.tie(NULL)->sync_with_stdio(false)
 using namespace std;
+#define ld long double
 #define ll long long
 #define pii pair<ll,ll>
+#define fastio cin.tie(NULL)->sync_with_stdio(false)
 #define x first
-#define y second
+#define y second 
 
 int CCW(pii a, pii b, pii c){
     ll T1 = a.x*b.y + b.x*c.y + c.x*a.y;
     ll T2 = a.y*b.x + b.y*c.x + c.y*a.x;
-    if(T1-T2 > 0) return 1;
-    else if(T1-T2 == 0) return 0;
+    if(T1-T2>0) return 1;
+    else if(T1-T2==0) return 0;
     else return -1;
 }
 
-vector<pii> ConvexHull(vector<pii> A){
-    int n = A.size(), k=0;
-    if(n<=2) return A;
-    vector<pii> ret(2*n);
-    sort(A.begin(), A.end());
-    for(int i=0; i<n; i++){
-        while(k>=2 && CCW(ret[k-2], ret[k-1], A[i])<=0) k--;
-        ret[k++] = A[i];
+int N;
+vector<pii> V;
+
+vector<int> ConvexHull(){
+    vector<int> st, en;
+    st.push_back(0); st.push_back(1);
+    for(int i=2; i<N;){
+        if(CCW(V[st[st.size()-2]], V[st[st.size()-1]], V[i]) < 0) st.push_back(i++);
+        else{
+            st.pop_back();
+            if(st.size()==1) st.push_back(i++);
+        }
     }
-    for(int i=n-2, t=k+1; i>=0; i--){
-        while(k>=t && CCW(ret[k-2], ret[k-1], A[i])<=0) k--;
-        ret[k++] = A[i];
+    en.push_back(N-1); en.push_back(N-2);
+    for(int i=N-3; i>=0;){
+        if(CCW(V[en[en.size()-2]], V[en[en.size()-1]], V[i]) < 0) en.push_back(i--);
+        else{
+            en.pop_back();
+            if(en.size()==1) en.push_back(i--);
+        }
     }
-    ret.resize(k-1);
+
+    vector<int> ret(st);
+    for(int i=1; i<en.size()-1; i++){
+        ret.push_back(en[i]);
+    }
     return ret;
 }
 
 int main(){
     fastio;
-    int N; cin>>N; 
-    vector<pii> V(N);
+    cin>>N; V.resize(N);
     for(int i=0; i<N; i++) cin>>V[i].x>>V[i].y;
     sort(V.begin(), V.end());
 
-    vector<pii> hulls = ConvexHull(V);
+    vector<int> hulls = ConvexHull();
 
     hulls.push_back(hulls[0]);
 
-    long long total = 0;
+    ld sum1=0, sum2=0;
     for(int i=0; i<hulls.size()-1; i++){
-        total += (hulls[i].x*hulls[i+1].y - hulls[i].y*hulls[i+1].x);
+        sum1 += (V[hulls[i]].x*V[hulls[i+1]].y);
+        sum2 += (V[hulls[i]].y*V[hulls[i+1]].x);
     }
-    cout<<total/100;
+    ld area = abs((sum1-sum2))/2;
+    ll ans = area/50;
+    cout<<ans;
     return 0;
 }
