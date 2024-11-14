ed23e3547708773944c9fb27f7e83eca068ddc54
jemin0619 <72261100+jemin0619@users.noreply.github.com>
2024-11-14 09:17:03 +0900

    Create B 9342 염색체 (정규표현식).cpp

---
+++ 랜디/2024-11-13/B 9342 염색체 (정규표현식).cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include <regex>
+using namespace std;
+#define fastio cin.tie(NULL)->sync_with_stdio(false)
+
+int main(){
+    fastio;
+    regex r("[A,B,C,D,E,F]?A+F+C+[A,B,C,D,E,F]?");
+    int tc; cin>>tc;
+    while(tc--){
+        string N; cin>>N;
+        if(regex_match(N, r)) cout<<"Infected!\n";
+        else cout<<"Good\n";
+    }
+    return 0;
+}
